ccf7d857703ca43f374e83bb61d1b7e399c67559
Memo Pinkus <65270128+memojpinkus@users.noreply.github.com>
2022-03-09 20:33:23 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from './components/navBar';
+import Footer from './components/footer';
+import QtyPicker from './components/qtyPicker';
+import Catalog from './components/catalog';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+function App() {
+  return(<div className = "App">
+          <NavBar></NavBar>
+          <h1>Hello world!</h1>
+          <Catalog></Catalog>
+          <Footer></Footer>
+        </div>);
+}
+
+export default App;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,9 @@
+import "./navBar.css";
+
+const NavBar = () => {
+    return(<div className="navBar">
+                <h2>This will be the NavBar!</h2>
+            </div> );
+};
+
+export default NavBar;
